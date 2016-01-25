b0174c0ebc6199357b1586335783f864e95b77d5
wendywang <wangwenusa11@gmail.com>
2016-01-24 20:54:48 -0800

    Created simple GET and POST request

--- HTTPServer.js
+++ HTTPServer.js
@@ -4,10 +4,34 @@ var http = require('http');
 //define a port
 const PORT = 8080;
 
+//server should respond differently to different URL paths. So we need a dispatcher
+var dispatcher = require('httpdispatcher');
+
 //handles requests and send response
 function handleRequest(request, response){
-    response.end('Path hit: ' + request.url);
+    try {
+        //log the request on console
+        console.log(request.url);
+        //dispatch
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
 }
+
+dispatcher.setStatic('waitingtime');
+
+//login GET request. Need to get the login page HTML from Joy
+dispatcher.onGet("/login", function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Default login page');
+});
+
+//login POST request
+dispatcher.onPost("/login", function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data from login');
+})
 
 //create a server
 var server  = http.createServer(handleRequest);
