7bb4bebda05be258b653c9102eb690a542555c09
Wendy Wang <wangwenusa11@gmail.com>
2016-03-13 20:14:07 -0700

    Added patient information

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {VisitComponent} from './visit.component';
+import {PatientComponent} from './patient.component';
 
 @Component({
     selector: 'my-app',
