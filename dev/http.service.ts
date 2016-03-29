eba3374ad84f4f53216a571550f950ba662d0487
Wendy Wang <wangwenusa11@gmail.com>
2016-03-29 14:05:02 -0700

    Added http

---
+++ dev/http.service.ts
@@ -0,0 +1,27 @@
+import {Http} from 'angular2/http';
+import {Injectable} from 'angular2/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
+@Injectable()
+export class HTTPService{
+	constructor(private http:Http){
+
+	}
+	getQuery(){
+		return this.http.get("http://www.localhost:3002/visit").map(res => res.json());
+	}
+	getEquipmentQuery(){
+		return this.http.get("http://www.localhost:3002/equipment").map(res => res.json());
+	}
+	getPQuery(){
+		return this.http.get("http://www.localhost:3002/patient").map(res => res.json());
+	}
+	getSQuery(){
+		return this.http.get("http://www.localhost:3002/staff").map(res => res.json());
+	}
+	//postPQuery(){
+	//	return this.http.post("http://www.localhost:3002/patient", JSON.stringify({"id": 1, "name": "Новые"}),{headers:headers}).map(res => res.json());
+	//}
+
+
+}
