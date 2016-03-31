963f1fc209730731d5b81395d4e0037cb37c85d6
Joshua DeJong <joshua@Joshuas-MacBook-Pro-2.local>
2016-03-31 14:42:51 -0700

    added files that I forgot to add

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,14 +1,23 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, Router} from 'angular2/router';
-import {Home} from "./dashboard";
+import {DashboardComponent} from "./dashboard.component";
 import {Login} from "./login";
 import {EquipmentComponent} from "./equipment.component";
 
 @Component({
     selector: 'my-app',
-    directives: [EquipmentComponent],
-    template: `<equipment></equipment>`
+    directives: [DashboardComponent],
+    template: `<dashboard></dashboard>`
 })
+
+@RouteConfig([
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+])
 
 
 export class AppComponent {
--- dev/dashboard.component.ts
+++ dev/dashboard.component.ts
@@ -10,40 +10,9 @@ import {isLoggedin} from "./is-loggedin";
 import {Authentication} from "./authentication";
 
 @Component({
-    selector: 'home',
+    selector: 'dashboard',
     directives: [],
-    template: `
-        <html>
-<head>
-    <title>Dashboard</title>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet"/>
-</head>
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="../home">ERWaitingTime</a>
-        <div class="container" align="right">
-        <button a href="#" class="btn btn-link" (click)="onLogout()">Logout</button>
-            </div>
-
-    </div>
-</nav>
-<br>
-<br>
-<br>
-<div align="center">
-    <h2>Welcome, you are successfully logged in.</h2>
-</div>
-    <a href="../views/patients.component.html" class="btn btn-link" align="right">Equipment Register</a>
-    <a href="../views/patients.component.html" class="btn btn-link" align="right">View Patients</a><br>
-    <a href="../views/visit.html" class="btn btn-link" align="right">Visit Page</a><br>
-    <a href="../views/visit.html" class="btn btn-link" align="right">View Patients</a><br>
-
-
-</body>
-</html>
-            
-`,
+    templateUrl: 'templates/dashboard.html'
 })
 
 
