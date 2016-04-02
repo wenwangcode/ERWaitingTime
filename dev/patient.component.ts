d3c9f93b1216a8ae36e82067ec14301e3fa9198a
Wendy Wang <wangwenusa11@gmail.com>
2016-04-02 12:24:04 -0700

    Added scripts for delete row

--- dev/patient.component.ts
+++ dev/patient.component.ts
@@ -60,4 +60,24 @@ export class PatientComponent{
             () => console.log("complete")
         );
     }
+    deleteRow(tableID) {
+    try {
+        var table = document.getElementById(tableID);
+        var rowCount = table.rows.length;
+
+        for(var i=0; i<rowCount; i++) {
+            var row = table.rows[i];
+            var chkbox = row.cells[0].childNodes[0];
+            if(null != chkbox && true == chkbox.checked) {
+                table.deleteRow(i);
+                rowCount--;
+                i--;
+            }
+
+
+        }
+    }catch(e) {
+        alert(e);
+    }
+}
 }
