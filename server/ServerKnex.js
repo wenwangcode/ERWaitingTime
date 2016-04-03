8d35426a803cbf23193f2b65e758af6f30a7ab1b
RichardChiang <rc@RichardChiangs-MacBook-Pro.local>
2016-04-02 17:47:54 -0700

    updated for selection, join, division, delete

--- server/ServerKnex.js
+++ server/ServerKnex.js
@@ -25,19 +25,41 @@ app.use(function(req,res,next){
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/visitpatient', function(req,res){visit_patient(req,res)})
+var selectAllTablesOptions = ['equipment','patient','visit'];
+var postTablesOptions = ['patient', 'equipment', 'vital', 'report', 'visit', 'staff',
+'prescription'];
 
-app.get('/equipment', function(req,res){getAllFromTable(req,res,'equipment');});
-app.get('/patient', function(req,res){getAllFromTable(req,res,'patient');});
-app.get('/visit', function(req,res){getAllFromTable(req,res,'visit');});
+// Join Visit and Patient table
+app.get('/patient_visit', function(req,res){visit_patient(req,res)})
 
-app.post('/patient',function (req,res){postData(req,res,'patient')});
-app.post('/equipment',function(req,res){postData(req,res,'equipment')});
-app.post('/vital',function(req,res){postData(req,res,'vital')});
-app.post('/report',function(req,res){postData(req,res,'report')});
-app.post('/visit',function(req,res){postData(req,res,'visit')});
-app.post('/staff',function(req,res){postData(req,res,'staff')});
-app.post('/prescription',function(req,res){postData(req,res,'prescription')});
+// Join Visit and Report table
+app.get('/patient_report', function(req,res){patient_report(req,res)})
+
+// Select experienced staff
+app.get('/staff/:year', function(req,res){selectExperiencedStaff(req,res,req.params.year)});
+
+// Get All from table
+app.get('/:table', function(req,res){processQuery(req,res, selectAllTablesOptions, getAllFromTable)});
+
+// Post to table
+app.post('/:table', function(req,res){processQuery(req,res,postTablesOptions, postData)});
+
+// Utilize all equipment
+app.get('/utilize_equip/:eidList', function(req,res){
+    var eids = req.params.eidList.split(',');
+    utilizeAllEquipment(req,res, eids);
+});
+
+// Delete patient based on pid
+app.get('/patient/delete/:id', function(req,res){deleteFromPatient(req,res)});
+
+function processQuery(req,res,options,handler){
+    if((options.indexOf(req.params.table)) > -1){
+         handler(req,res,req.params.table);
+    } else {
+         res.end(JSON.stringify({error: 'invalid table name'}));
+    }
+}
 
 
 function getAllFromTable(req,res,table){
@@ -48,22 +70,55 @@ function postData(req,res,table){
     var post = JSON.parse(req.body.json);
     knex(table).insert(post)
         .catch(this.errorHandler)
-        .return({success:true}); 
+        .then({success:true}); 
+}
+
+function deleteFromPatient(req,res){
+    knex('patient')
+        .where('pid',req.params.id)
+        .del()
+        .catch(this.errorHandler)
+        .then(res.send(JSON.stringify({success:true})));
 }
 
 function visit_patient(req,res){
-    knex.from('visit').innerJoin('patient', 'visit.pid', 'patient.pid')
-    .select('patient.p_lname',
-            'patient.p_fname',
-            'patient.pid',
-            'visit.room',
-            'visit.visit_date',
-            'visit.sid')
+    knex.from('patient').innerJoin('visit', 'visit.pid', 'patient.pid')
+    .select()
     .catch(this.errorHandler).then(rows => res.send(rows));
+}
+
+function patient_report(req,res){
+    knex.from('patient').innerJoin('report', 'report.pid', 'patient.pid')
+    .select()
+    .catch(this.errorHandler).then(rows => res.send(rows));
+}
+
+function selectExperiencedStaff(req,res,year){
+    if(year >= 30 && year <= 50){
+        knex('staff').where('experience_in_years','>',year).select('s_lname','s_fname').catch(this.errorHandler).then(rows => res.send(rows));
+    } else {
+        invalidCondition(res,'year must be between 30 and 50 inclusively');
+    }
+}
+
+
+
+function utilizeAllEquipment(req,res,eids){
+    knex('utilize')
+    .whereIn('eid', eids)
+    .groupBy('pid')
+    .havingRaw('count(*) = ?', eids.length)
+    .select('pid')
+    .catch(this.errorHandler)
+    .then(rows => res.send(rows))
 }
 
 function errorHandler(error){
     console.error(error);
+}
+
+function invalidCondition(res,msg){
+    res.send(JSON.stringify({error: msg}));
 }
 
 
