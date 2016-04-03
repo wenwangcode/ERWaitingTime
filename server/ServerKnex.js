c759c54994564ca5e447037d9bab370ab1e3e6cf
Wendy Wang <wangwenusa11@gmail.com>
2016-04-02 17:00:22 -0700

    Fixed staff sid bug

--- server/ServerKnex.js
+++ server/ServerKnex.js
@@ -6,7 +6,7 @@ var knex = require('knex')({
     connection: {
          host     : 'localhost',
          user     : 'root',
-         password : 'thematrix',
+         password : '639288',
          database : 'emergency'
     },
     pool:{
